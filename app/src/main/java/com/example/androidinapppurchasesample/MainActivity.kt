6b50ee9a066f86357ad10533e5efce19ff761f00
ktakino <kakeru.takino@robinsons.co.jp>
2023-09-29 15:28:10 +0900

    課金未実装

--- app/src/main/java/com/example/androidinapppurchasesample/MainActivity.kt
+++ app/src/main/java/com/example/androidinapppurchasesample/MainActivity.kt
@@ -1,15 +1,31 @@
 package com.example.androidinapppurchasesample
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androidinapppurchasesample.ui.theme.AndroidInAppPurchaseSampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +33,80 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AndroidInAppPurchaseSampleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MessageCard(Message("Android", "Jetpack Compose"))
                 }
             }
         }
     }
 }
 
+data class Message(val author: String, val body: String)
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MessageCard(msg: Message) {
+    // パディング追加
+    Row(Modifier.padding(all = 8.dp)) {
+        Image(painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription = "Content profile picture",
+            modifier = Modifier
+                // 画像サイズ
+                .size(40.dp)
+                // 画像を丸で切り抜き
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.secondary, CircleShape))
+
+        // 画像と列の間に水平スペースを入れる
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(text = msg.author,
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                style = MaterialTheme.typography.titleMedium)
+            // 間に垂直スペースを入れる
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+                Text(
+                    text = msg.body,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    AndroidInAppPurchaseSampleTheme {
-        Greeting("Android")
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
     }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    AndroidInAppPurchaseSampleTheme {
+        Conversation(messages = SampleData.conversationSample)
+    }
+}
+
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+@Composable
+fun PreviewMessageCard() {
+    AndroidInAppPurchaseSampleTheme {
+        Surface {
+            MessageCard(
+                msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
+            )
+        }
+    }
+
 }
